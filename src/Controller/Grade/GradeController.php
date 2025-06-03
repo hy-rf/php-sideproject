e2292b1ca2cc74f55f8fab49db6486eea6086c44
hy-rf <heyu880202@gmail.com>
2025-06-03 11:06:53 +0800

    feat: remove unused controllers and add controllers needed

---
+++ src/Controller/Grade/GradeController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Controller\Grade;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class GradeController
+{
+    private Request $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function handle(): Response
+    {
+        switch ($this->request->getMethod()) {
+            case "GET":
+                return $this->get();
+            case "POST":
+                return $this->post();
+            case "PUT":
+                return $this->put();
+            case "DELETE":
+                return $this->delete();
+            default:
+                return new Response(
+                    "Method Not Allowed",
+                    Response::HTTP_METHOD_NOT_ALLOWED
+                );
+        }
+    }
+
+    private function get(): Response
+    {
+        $data = [
+            "message" => "GET successful",
+            "timestamp" => time(),
+        ];
+
+        return new Response(json_encode($data), Response::HTTP_OK, [
+            "Content-Type" => "application/json",
+        ]);
+    }
+
+    private function post(): Response
+    {
+        $data = json_decode($this->request->getContent(), true);
+
+        if (!$data || !isset($data["content"])) {
+            return new Response(
+                json_encode(["error" => "Invalid input. Content is required."]),
+                Response::HTTP_BAD_REQUEST,
+                ["Content-Type" => "application/json"]
+            );
+        }
+
+        $responseData = [
+            "message" => "POST reply successful",
+            "content" => $data["content"],
+        ];
+
+        return new Response(
+            json_encode($responseData),
+            Response::HTTP_CREATED,
+            ["Content-Type" => "application/json"]
+        );
+    }
+
+    private function put(): Response
+    {
+        $data = json_decode($this->request->getContent(), true);
+
+        if (!isset($data["id"])) {
+            return new Response(
+                json_encode(["error" => "Id is empty"]),
+                Response::HTTP_BAD_REQUEST,
+                ["Content-Type" => "application/json"]
+            );
+        }
+        if (!$data || !isset($data["content"])) {
+            return new Response(
+                json_encode(["error" => "Invalid input. Content is required."]),
+                Response::HTTP_BAD_REQUEST,
+                ["Content-Type" => "application/json"]
+            );
+        }
+
+        $responseData = [
+            "message" => "POST reply successful",
+            "content" => $data["content"],
+        ];
+
+        return new Response(
+            json_encode($responseData),
+            Response::HTTP_CREATED,
+            ["Content-Type" => "application/json"]
+        );
+    }
+
+    private function delete(): Response
+    {
+        $data = json_decode($this->request->getContent(), true);
+
+        if (!isset($data["id"])) {
+            return new Response(
+                json_encode(["error" => "Id is empty"]),
+                Response::HTTP_BAD_REQUEST,
+                ["Content-Type" => "application/json"]
+            );
+        }
+        if (!$data || !isset($data["content"])) {
+            return new Response(
+                json_encode(["error" => "Invalid input. Content is required."]),
+                Response::HTTP_BAD_REQUEST,
+                ["Content-Type" => "application/json"]
+            );
+        }
+
+        $responseData = [
+            "message" => "POST reply successful",
+            "content" => $data["content"],
+        ];
+
+        return new Response(
+            json_encode($responseData),
+            Response::HTTP_CREATED,
+            ["Content-Type" => "application/json"]
+        );
+    }
+}
--- src/index.php
+++ src/index.php
@@ -4,18 +4,30 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Controller\Post\PostController;
-use Controller\Reply\ReplyController;
+use Controller\Auth\AuthController;
+use Controller\Grade\GradeController;
+use Controller\Subject\SubjectController;
+use Controller\Class\ClassController;
+use Controller\Student\StudentController;
 
 $request = Request::createFromGlobals();
 $path = $request->getPathInfo();
 
 switch ($path) {
-    case "/post":
-        $controller = new PostController($request);
+    case "/auth":
+        $controller = new AuthController($request);
         break;
-    case "/reply":
-        $controller = new ReplyController($request);
+    case "/grade":
+        $controller = new GradeController($request);
+        break;
+    case "/subject":
+        $controller = new SubjectController($request);
+        break;
+    case "/class":
+        $controller = new ClassController($request);
+        break;
+    case "/student":
+        $controller = new StudentController($request);
         break;
     default:
         $response = new Response("Not Found", Response::HTTP_NOT_FOUND);
